2ca90470bc642d24dfa50d037675fef132995baf
TruongAnim <truonganim.dev@gmail.com>
2023-04-13 12:24:59 +0700

    add sign up ui

---
+++ lib/core/helper/assets_helper.dart
@@ -0,0 +1,4 @@
+class AssetsHelper {
+  static const String imagePath = 'assets/images/';
+  static const String avatar = '${imagePath}avatar.png';
+}
---
+++ lib/views/screens/auth/sign_up_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/core/helper/assets_helper.dart';
+import 'package:tiktok_clone/views/widgets/button_widget.dart';
+import 'package:tiktok_clone/views/widgets/input_text_field.dart';
+
+class SignUpScreen extends StatelessWidget {
+  SignUpScreen({super.key});
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'TikTok Clone',
+                style: TextStyle(
+                    fontSize: 35,
+                    color: buttonColor,
+                    fontWeight: FontWeight.w900),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text(
+                'Register',
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: const CircleAvatar(
+                      foregroundImage: AssetImage(
+                        AssetsHelper.avatar,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    right: -10,
+                    child: Container(
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.add_a_photo,
+                          size: 30,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                child: InputTextField(
+                    controller: usernameController,
+                    label: 'Username',
+                    icon: Icons.person),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                child: InputTextField(
+                    controller: emailController,
+                    label: 'Email',
+                    icon: Icons.email),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin: const EdgeInsets.symmetric(horizontal: 25),
+                child: InputTextField(
+                  controller: passwordController,
+                  label: 'Password',
+                  icon: Icons.lock,
+                  isObsecure: true,
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: ButtonWidget(
+                  onTap: () {},
+                  title: 'Login',
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already have an account? ',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(fontSize: 18, color: buttonColor),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
